acb55815ef82fe100badb98ca1dd810f5a720ddc
TestDp <Cristianmg13@hotmail.com>
2017-11-29 13:27:54 -0500

    creacion de controlador de historias clinicas

---
+++ HC_SB/Controllers/HistoriasClinicas/HistoriasClinicasController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HC_SB.Controllers.HistoriasClinicas
+{
+    public class HistoriasClinicasController : Controller
+    {
+        // GET: HistoriasClinicas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BuscarHistoria() 
+        {
+            return View();
+        }
+    }
+}
