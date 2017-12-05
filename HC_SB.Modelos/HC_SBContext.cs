f864921e8e95a15b643bdc096babc4d9da2d932f
TestDp <Cristianmg13@hotmail.com>
2017-12-04 20:36:03 -0500

    se crean contexto y primer modelo

---
+++ HC_SB.Modelos/HC_SBContext.cs
@@ -0,0 +1,32 @@
+﻿namespace HC_SB.Modelos
+{
+    using HC_SB.Modelos.HistoriasClinicas;
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class HC_SBContext : DbContext
+    {
+        // El contexto se ha configurado para usar una cadena de conexión 'HC_SBContext' del archivo 
+        // de configuración de la aplicación (App.config o Web.config). De forma predeterminada, 
+        // esta cadena de conexión tiene como destino la base de datos 'HC_SB.Modelos.HC_SBContext' de la instancia LocalDb. 
+        // 
+        // Si desea tener como destino una base de datos y/o un proveedor de base de datos diferente, 
+        // modifique la cadena de conexión 'HC_SBContext'  en el archivo de configuración de la aplicación.
+        public HC_SBContext()
+            : base("name=HC_SBContext")
+        {
+        }
+
+        // Agregue un DbSet para cada tipo de entidad que desee incluir en el modelo. Para obtener más información 
+        // sobre cómo configurar y usar un modelo Code First, vea http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<HistoriaClinica> Tbl_HistoriasClinicas { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ HC_SB.Modelos/HistoriasClinicas/HistoriaClinica.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="Peligro.cs" company="Ada SA">
+// Copyright (c) 2017 All Rights Reserved
+// </copyright>
+// <author>Cristian Mauricio Arenas Gomez.</author>
+// <date>06/01/2017</date>
+// <summary>Modelo que contiene la informacion de los peligros registrados.</summary>
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HC_SB.Modelos.HistoriasClinicas
+{
+
+    [Table("Tbl_HistoriasClinicas")]
+    public class HistoriaClinica
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Key]
+        public int PKHistoriaClinica { get; set; }
+
+
+        ///// <summary>
+        ///// Obtiene y establece la clave foranea a la tabla  sede.
+        ///// </summary>
+        //[ForeignKey("Sede")]
+        //public int FK_Sede { get; set; }
+
+        ///// <summary>
+        ///// Obtiene y establece un objeto de sede.
+        ///// </summary>
+        //[ForeignKey("Pk_Id_Sede")]
+        //public virtual Sede Sede { get; set; }
+    }
+}
