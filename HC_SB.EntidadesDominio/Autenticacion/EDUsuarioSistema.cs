06f56b20b835b40f4f06af15e76fb1d956b4cda1
TestDp <Cristianmg13@hotmail.com>
2017-12-07 11:08:45 -0500

    visualizacion de lista de usuarios

---
+++ HC_SB.EntidadesDominio/Autenticacion/EDUsuarioSistema.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HC_SB.EntidadesDominio.Autenticacion
+{
+    public class EDUsuarioSistema
+    {
+        public string IDUser { get; set; }
+
+        
+        public string userName { get; set; }
+
+        
+        public string nombreCompleto { get; set; }
+
+        
+        public string rol { get; set; }
+
+        
+    }
+}
--- HC_SB/Controllers/AdminUsuariosYRolesController.cs
+++ HC_SB/Controllers/AdminUsuariosYRolesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HC_SB.EntidadesDominio.Autenticacion;
+using HC_SB.Modelos.Autenticacion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,36 @@ namespace HC_SB.Controllers
 {
     public class AdminUsuariosYRolesController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: AdminUsuariosYRoles
         public ActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// se muestra todos los usuarios del sistema
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult listaUsuarios()
+        {
+
+            var users = db.Users.ToList();
+            List<EDUsuarioSistema> listaUsuarios = new List<EDUsuarioSistema>();
+            foreach (var user in users)
+            {
+
+                var EDUsuarioSistema = new EDUsuarioSistema
+                {
+                    IDUser = user.Id,
+                    userName = user.UserName,
+                    nombreCompleto = user.nombreCompleto 
+                };
+                listaUsuarios.Add(EDUsuarioSistema);
+
+            }
+
+            return View(listaUsuarios);
         }
     }
 }
